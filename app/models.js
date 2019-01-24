52497c9044807c7f2d9718cde510803125262b88
RayWiis <rejus.paukst@gmail.com>
2019-01-24 22:31:54 +0200

    Add minimal validation for location

--- app/models.js
+++ app/models.js
@@ -1,4 +1,5 @@
 const Sequelize = require('sequelize')
+const validator = require('validator')
 
 const database = new Sequelize({
     dialect: 'sqlite',
@@ -30,7 +31,27 @@ const Item = database.define('Item', {
     },
     location: {
         type: Sequelize.TEXT,
-        allowNull: false
+        allowNull: false,
+        validate: {
+            isLocation(value) {
+                if (typeof value !== 'string') {
+                    throw 'Location should be a string'
+                }
+
+                const values = value.split(',')
+                values.map((value) => {
+                    if (!value) {
+                        throw 'Not all fields are defined in location'
+                    }
+                })
+
+                const [ country, city, street, lat, lon ] = values
+                validator.isAlpha(country)
+                validator.isAlpha(city)
+                validator.isDecimal(lat)
+                validator.isLatLong(`${lat}, ${lon}`)
+            }
+        }
     }
 })
 
