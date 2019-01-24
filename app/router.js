113c10f019a71ede7ba8a17a786b56a0bd16ac21
RayWiis <rejus.paukst@gmail.com>
2019-01-24 19:56:52 +0200

    Add route to update item

--- app/router.js
+++ app/router.js
@@ -27,6 +27,34 @@ router.get('/api/item', async (req, res) => {
     }
 })
 
+router.put('/api/item/:item_id', async (req, res) => {
+    const { title, description, stock, price, location } = req.body
+
+    try {
+        const items = await models.Item.findAll({
+            where: { id: req.params.item_id }
+        })
+
+        if (items.length <= 0) {
+            throw `No item with id ${req.params.id}`
+        }
+
+        let item = items[0]
+
+        item.title = title || item.title
+        item.description = description || item.description
+        item.stock = stock || item.stock
+        item.price = price || item.price
+        item.location = location || item.location
+
+        await item.save()
+
+        res.send({ item })
+    } catch (err) {
+        res.send({ err })
+    }
+})
+
 module.exports = (async () => {
     models = await require('./models')
     return router
